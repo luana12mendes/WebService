65a8df89e332fe8a29e8a05b1821ffb2ff0a8c08
luana12mendes <luana12mendes@gmail.com>
2017-07-24 00:56:45 -0300

    atualização

--- app/src/main/java/com/example/luana/webservice/MainActivity.java
+++ app/src/main/java/com/example/luana/webservice/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.luana.webservice;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView webService;
+    String uri = "Http://swapi.co/api/people/1/";
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] items = {"Ir ao mercado", "Ir ao Dentista", "Pagar as Contas", "Ir ao shopping"};
-        ListView listaSimples = (ListView) findViewById(R.id.listaSimplesId);
-        ArrayAdapter<String> adapterSimples = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
-        listaSimples.setAdapter(adapterSimples);
+        url = URI.create(uri).toString();
+        String resultado= "";
+        webService = (TextView) findViewById(R.id.Web);
+
+        BuscaTask busca = new BuscaTask(url,null,resultado);
+        busca.execute();
+
+    }
+
+    public class BuscaTask extends AsyncTask<String, Void, String>{
+
+        ProgressDialog dialog;
+
+        public BuscaTask(String url, Object o, String resultado) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String jsonObject) {
+
+            if (jsonObject != null) {
+                webService.setText(jsonObject);
+            }
+
+            dialog.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return HTTPUtils.acessar(url);
+        }
     }
 }
